b904ba2a655ec6f755ea450611ad7e973175e6b6
MikuroXina <ryosukadnak@gmail.com>
2023-04-29 15:53:42 +0900

    Add play_game

--- src/rule.rs
+++ src/rule.rs
@@ -1,6 +1,94 @@
+use rand::{rngs::SmallRng, SeedableRng};
+
+use self::{
+    card::Gem,
+    chip::{Chip, ChipStack},
+    game_board::{GameBoard, OpenCard},
+    player_board::PlayerBoard,
+};
+
 pub mod card;
 pub mod chip;
 pub mod deck;
 pub mod game_board;
 pub mod noble;
 pub mod player_board;
+
+/// Plays the game and returns a position of the winner.
+pub fn play_game<const N: usize>(mut players: [Box<dyn Player>; N], seed: u64) -> usize {
+    let mut rng = SmallRng::seed_from_u64(seed);
+    let mut game_board = GameBoard::new(
+        players
+            .len()
+            .try_into()
+            .expect("Splendor is only for 2-4 players"),
+        &mut rng,
+    );
+    let mut player_boards = [(); N].map(|_| PlayerBoard::new());
+    const SCORE_THRESHOLD: u8 = 15;
+    while player_boards
+        .iter()
+        .any(|player| SCORE_THRESHOLD <= player.score())
+    {
+        for (player_board, player) in player_boards.iter_mut().zip(players.iter_mut()) {
+            player_turn(player.as_mut(), player_board, &mut game_board);
+        }
+    }
+    player_boards
+        .iter()
+        .enumerate()
+        .max_by_key(|(_, board)| board.score())
+        .map(|(idx, _)| idx)
+        .unwrap()
+}
+
+fn player_turn(
+    player: &mut dyn Player,
+    player_board: &mut PlayerBoard,
+    game_board: &mut GameBoard,
+) {
+    match player.ask_choice(player_board, game_board) {
+        Choice::ThreeDifferentChips(gems) => {
+            game_board.pick_differ_three_chips(gems);
+            player_board.bring_differ_three_chips(gems);
+        }
+        Choice::TwoSameChips(gem) => {
+            game_board.pick_same_two_chips(gem);
+            player_board.bring_same_two_chips(gem);
+        }
+        Choice::Buy(target) => {
+            for use_golden_chips in [false, true] {
+                if !player_board.can_buy(target.as_ref(), use_golden_chips) {
+                    continue;
+                }
+                let picked = game_board.pick_open_card(target);
+                let paid = player_board.buy(picked, false);
+                game_board.return_chips(paid);
+            }
+        }
+        Choice::Keep(card) => {
+            let picked = game_board.pick_open_card(card);
+            player_board.keep(picked);
+            if 0 < game_board.bank().chips(Chip::Golden) {
+                game_board.pick_golden_chip();
+                player_board.bring_golden_chip();
+            }
+        }
+        Choice::Nothing => {}
+    }
+}
+
+pub trait Player {
+    fn ask_choice(&mut self, your: &PlayerBoard, game: &GameBoard) -> Choice;
+
+    fn force_drop(&self, your: &PlayerBoard) -> ChipStack;
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum Choice<'a> {
+    ThreeDifferentChips([Gem; 3]),
+    TwoSameChips(Gem),
+    Buy(OpenCard<'a>),
+    Keep(OpenCard<'a>),
+    Nothing,
+}
