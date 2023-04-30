349fcbbc3129b566665f17138252e4139c467af2
MikuroXina <ryosukadnak@gmail.com>
2023-04-30 10:16:23 +0900

    Add GemKnight

--- src/bot.rs
+++ src/bot.rs
@@ -1,1 +1,23 @@
+use crate::rule::{
+    card::{Card, Gem},
+    chip::ChipStack,
+    game_board::GameBoard,
+    player_board::PlayerBoard,
+    Choice, Player,
+};
 
+#[derive(Debug, Default, Clone)]
+pub struct GemKnight {
+    target_gem: Option<Gem>,
+    target_card: Option<Card>,
+}
+
+impl Player for GemKnight {
+    fn ask_choice(&mut self, your: &PlayerBoard, game: &GameBoard) -> Choice {
+        todo!()
+    }
+
+    fn force_drop(&self, _your: &PlayerBoard) -> ChipStack {
+        todo!()
+    }
+}
